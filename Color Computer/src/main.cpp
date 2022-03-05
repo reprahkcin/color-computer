d66e981b464dd0df1f1eff3222bc6df41cc804d2
Nick Harper <bignickharper@gmail.com>
2022-03-04 22:12:25 -0800

    installed libraries

--- Color Computer/src/main.cpp
+++ Color Computer/src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
 
-void setup() {
+// library for LCD
+// library for RGB displays
+// library for color sensor
+
+// LCD PINS
+
+// RGB DISPLAY PINS
+
+// COLOR SENSOR PINS
+
+// BUTTON PINS
+
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
